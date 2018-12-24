5a5d8c9adc95e9405bfc21ac4451bfcff53d2b7a
Jaysingh <38240744+Anticsss@users.noreply.github.com>
2018-12-24 15:54:43 +0530

    Create predict.py

---
+++ next_word/predict.py
@@ -0,0 +1,18 @@
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+
+with open('model/MLE.pickle', 'rb') as f:
+    # The protocol version used is detected automatically, so we do not
+    # have to specify it.
+    mle = pickle.load(f)
+
+while True:             
+    word = input("Enter a word: ")
+    if word == "q":       
+        break
+    else:
+        next = mle.generate(text_seed=[word], random_seed=5)
+        score = mle.score(word)
+        print("Next word is :",next)
+        print("Score is :",score)
